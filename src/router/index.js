f301534c7de88b8590521ebf229626667ae859b3
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-22 20:44:36 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import BlogView from "../views/BlogView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,7 +15,17 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/AllBlogsView.vue"),
+    },
+    {
+      path: "/blog/:id",
+      name: "BlogView",
+      component: BlogView,
+    },
+    {
+      path: "/addpost",
+      name: "AddPost",
+      component: () => import("../views/AddPostView.vue"),
     },
   ],
 });
