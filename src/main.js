d9ebf005f9df08859377dfd82e070d40cf4621d8
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-22 12:47:28 +0530

    Update main.js
    imported pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 
 import "./assets/main.css";
 
+const pinia = createPinia();
+
 const app = createApp(App);
+
+app.use(pinia);
 
 app.use(router);
 
