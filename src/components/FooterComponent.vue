51cf6ef7d173fa46891a8cdef906ca5352fe48fe
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-24 16:26:26 +0530

    Update FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,16 +1,18 @@
 <template>
   <footer class="mt-16 flex flex-col items-center">
     <div class="mb-3 flex space-x-4">
-      <div class="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+      <div
+        class="mb-2 flex flex-wrap space-x-2 text-sm text-gray-500 dark:text-gray-400"
+      >
         <div>Swaubhik</div>
         <div>•</div>
         <div>&copy; {{ new Date().getFullYear() }}</div>
         <div>•</div>
         <router-link to="/">VueBlog</router-link>
       </div>
-      <div class="mb-8 text-sm text-gray-500 dark:text-gray-400">
-        <a target="_blank" href="#"> Tailwind Vuejs Starter Blog </a>
-      </div>
+    </div>
+    <div class="mb-8 text-sm text-gray-500 dark:text-gray-400">
+      <a target="_blank" href="#"> Tailwind Vuejs Starter Blog </a>
     </div>
   </footer>
 </template>
