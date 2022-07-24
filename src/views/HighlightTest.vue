7b87dc334b82205e7cdffb3c8fe0035202c74f71
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-25 01:12:31 +0530

    Update HighlightTest.vue
    added highlight feature

--- src/views/HighlightTest.vue
+++ src/views/HighlightTest.vue
@@ -1,45 +1,51 @@
 <template>
   <div>
-    <img src="./assets/logo.png" />
+    <button
+      class="bg-primary-500 px-4 py-2 rounded-lg mb-8 text-gray-900"
+      @click="highlight"
+    >
+      highlight
+    </button>
     <div>
-      Search:
-      <input
-        class="text-black"
-        type="text"
-        v-model="query"
-        placeholder="Search"
-      />
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
+      delectus ipsum tempore odio ullam vel? Facere ab, iusto eius recusandae
+      tempore facilis unde harum, cum repudiandae, ut maiores? Dolor, earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam debitis
+      architecto eius reiciendis provident illo sint neque ullam excepturi
+      pariatur commodi ipsum dolor, quisquam dolores nemo cumque quam saepe
+      quaerat.
     </div>
-    <br />
-    <div v-html="highlight()"></div>
   </div>
 </template>
 
 <script>
+import rangy from "rangy";
+import "rangy/lib/rangy-highlighter";
+import "rangy/lib/rangy-classapplier";
 export default {
-  name: "app",
-  data() {
-    return {
-      query: "",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dignissim leo massa, sed aliquet leo posuere ut. Curabitur malesuada accumsan erat, id varius eros tincidunt quis. Vivamus lobortis, odio at fermentum efficitur, risus est tincidunt nisl, eget condimentum tellus dui vitae nibh. Donec id lorem condimentum, porttitor augue in, fermentum leo. Morbi condimentum, nunc ut malesuada placerat, sem nulla condimentum purus, iaculis tristique metus diam lobortis enim. Suspendisse potenti. Quisque urna magna, porta eget erat ac, pharetra vehicula erat. Suspendisse sed nisi ex.",
-    };
-  },
+  mounted() {},
   methods: {
     highlight() {
-      if (!this.query) {
-        return this.content;
+      var highlighter;
+
+      rangy.init();
+      highlighter = rangy.createHighlighter();
+      highlighter.addClassApplier(rangy.createClassApplier("highlight"));
+      const alphaNumeric = new RegExp(/^[a-zA-Z0-9]+$/);
+      let string = rangy.getSelection().toString();
+      if (alphaNumeric.test(string)) {
+        highlighter.highlightSelection("highlight");
+        // var selTxt = rangy.getSelection();
+        // console.log("selTxt: " + selTxt);
+        rangy.getSelection().removeAllRanges();
       }
-      return this.content.replace(new RegExp(this.query, "gi"), (match) => {
-        return '<span class="highlightText">' + match + "</span>";
-      });
     },
   },
 };
 </script>
 
 <style>
-.highlightText {
+.highlight {
   background: rgb(5, 91, 97);
   color: white;
   font-weight: bold;
