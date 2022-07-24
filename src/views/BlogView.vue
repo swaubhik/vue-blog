7686238b7f251288a82a5fb58bd5d3671e2c230e
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-07-24 16:29:27 +0530

    Update BlogView.vue
    Added Query search highlight function

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -49,12 +49,54 @@
                 </li>
               </ul>
             </dd>
+
+            <div class="relative mt-5 max-w-lg">
+              <input
+                v-model="search"
+                aria-label="Search Keywords"
+                type="text"
+                placeholder="Search Keywords"
+                class="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+              /><svg
+                class="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
           </dl>
           <div
             class="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0"
           >
+            <!-- <div>
+              Safe:
+              <span>
+                <template v-for="result in highlights" :key="result">
+                  <template v-if="result.match"
+                    ><strong class="bg-primary-500">{{
+                      result.text
+                    }}</strong></template
+                  >
+                  <template v-else>{{ result.text }}</template>
+                </template>
+              </span>
+            </div> -->
             <div
+              v-if="search == ''"
               v-html="post.content"
+              class="prose max-w-none pt-10 pb-8 dark:prose-dark"
+            ></div>
+            <div
+              v-else
+              v-html="formatedHTML"
               class="prose max-w-none pt-10 pb-8 dark:prose-dark"
             ></div>
           </div>
@@ -69,7 +111,7 @@
             <div class="pt-4 xl:pt-8">
               <router-link
                 class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                :to="{ name: 'Blogs' }"
+                :to="{ name: 'EditBlog', params: { id: post.id } }"
               >
                 Edit blog &rarr;</router-link
               >
@@ -90,15 +132,61 @@ import { useRoute } from "vue-router";
 
 export default {
   setup() {
-    const { post, loading, error } = storeToRefs(usePostStore());
+    const { post } = storeToRefs(usePostStore());
     const { fetchPost } = usePostStore();
     fetchPost(useRoute().params.id);
     return {
       formatDate,
       post,
-      loading,
-      error,
     };
+  },
+  data() {
+    return {
+      source: this.post.content,
+      search: "",
+    };
+  },
+  computed: {
+    formatedHTML() {
+      const regexp = new RegExp(this.search, "ig");
+      const highlights = this.source.replace(
+        regexp,
+        "<strong style='background-color:#14b8a6'>$&</strong>"
+      );
+      return `<span class="highlightText">${highlights}</span>`;
+    },
+    highlights() {
+      const results = [];
+      if (this.search && this.search.length > 0) {
+        const regexp = new RegExp(this.search, "ig");
+
+        let start = 0;
+        for (let match of this.source.matchAll(regexp)) {
+          results.push({
+            text: this.source.substring(start, match.index),
+            match: false,
+          });
+          start = match.index;
+          results.push({
+            text: this.source.substr(start, this.search.length),
+            match: true,
+          });
+          start += this.search.length;
+        }
+
+        if (start < this.source.length)
+          results.push({ text: this.source.substring(start), match: false });
+      }
+
+      if (results.length === 0) {
+        results.push({
+          text: this.source,
+          match: false,
+        });
+      }
+
+      return results;
+    },
   },
 };
 </script>
